9289ea61b564d25e12f19c0880ad7fd8b29be7ec
DENYS BUKHTAROV <denis180984@gmail.com>
2023-05-18 21:50:52 +0300

    index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/theme';
 import './scss/app.scss';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

fb3180ca65be05bf546e556ae7481238288dec0a
DENYS BUKHTAROV <denis180984@gmail.com>
2023-05-18 21:46:02 +0300

    index
    Package JSON

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import './scss/app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/your-pet">
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
+      </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );

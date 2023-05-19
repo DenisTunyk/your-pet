d0bf7b4c129f0db98862013b986c8614ca202c0f
VadimKorobec <_s_e_t__@ukr.net>
2023-05-19 15:54:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-
 import { Header } from './Header/Header';
 import { Home } from '../pages/Home/Home';
 import { NotFound } from '../pages/NotFound/NotFound';
 import { OurFriend } from '../pages/OurFriend/OurFriend';
-import LoginPage  from '../pages/LoginPage/LoginPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
 import { FindPet } from '../pages/FindPet/FindPet';
 import { News } from '../pages/News/News';
 import { Profile } from '../pages/Profile/Profile';
 import { NoticesPage } from '../pages/NoticesPage/NoticesPage';
+import { AuthForm } from './AuthForm/AuthForm';
 
 export const App = () => {
   return (
@@ -29,6 +29,7 @@ export const App = () => {
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <AuthForm />
     </div>
   );
 };

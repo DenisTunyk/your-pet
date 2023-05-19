146711d13ba8a57cfd060e0e549a54153b525265
VadimKorobec <_s_e_t__@ukr.net>
2023-05-19 17:08:48 +0300

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -2,3 +2,15 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = '';
+
+export const register = createAsyncThunk(
+  'autn/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const responce = await axios.post('/users/signup', credentials);
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);

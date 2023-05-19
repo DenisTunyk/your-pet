1ba5992a15534829ce1e1c0d518dbb2d4c320993
VadimKorobec <_s_e_t__@ukr.net>
2023-05-19 17:08:45 +0300

    Update auth-slice.js

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,5 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  user: {},
+  user: { email: null, password: null },
+  token: null,
+  isLoggedIn: false,
 };
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {},
+});
+
+export const authReducer = authSlice.reducer;

d545783d3f05524d307d3fef6b431f51ce1e3258
VadimKorobec <_s_e_t__@ukr.net>
2023-05-20 16:32:08 +0300

    Update auth-slice.js

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { register, logIn } from './auth-operations';
+import { register, logIn, logOut } from './auth-operations';
 
 const initialState = {
   user: { email: null, password: null },
@@ -20,6 +20,11 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
+    },
+    [logOut.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
     },
   },
 });

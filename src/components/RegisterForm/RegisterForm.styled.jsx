9463ecb9dd3cf4bb74547b411d5be1d8573fc2b1
VadimKorobec <_s_e_t__@ukr.net>
2023-05-20 17:08:37 +0300

    Update RegisterForm.styled.jsx

--- src/components/RegisterForm/RegisterForm.styled.jsx
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -12,8 +12,54 @@ export const Container = styled.div`
 export const Title = styled.h1`
   font-size: 24px;
   line-height: 33px;
-
   align-items: center;
   text-align: center;
+  margin-bottom: 40px;
   color: #111111;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin-bottom: 24px;
+`;
+
+export const Input = styled.input`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 16px;
+  gap: 10px;
+  width: 100%;
+  height: 48px;
+  border: 1px solid #54adff;
+  border-radius: 40px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 48px;
+  padding: 10px 75px;
+  background: #54adff;
+  border-radius: 40px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 27px;
+`;
+
+export const Span = styled.span`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  display: block;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.04em;
 `;

a1a9dd3e46b6608e38de8f68915e53ff98bbb847
VadimKorobec <_s_e_t__@ukr.net>
2023-05-20 17:28:09 +0300

    Update RegisterForm.styled.jsx

--- src/components/RegisterForm/RegisterForm.styled.jsx
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 280px;
+  width: 100%;
   height: 473px;
   border-radius: 20px;
   box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);

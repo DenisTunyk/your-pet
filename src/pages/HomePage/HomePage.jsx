fdf46345b0d0eba5fd0d1d6755c6f5937a535ecb
Dagaz <wlada801@gmail.com>
2023-05-21 23:11:07 +0300

    add /main

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import css from '../HomePage/HomePage.module.css';
+
+export const HomePage = () => {
+  return (
+    <>
+      <div className={css.page}>
+        <h1 className={css.h1}>
+          Take good care of <br />
+          your small pets
+        </h1>
+      </div>
+    </>
+  );
+};
+
+// export default HomePage;

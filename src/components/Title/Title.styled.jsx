379b47f22cbf0b3f0bb17eaa61e2929ff612b101
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2023-05-23 20:43:25 +0300

    fix style

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -1,20 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled('div')`
+export const Wrapper = styled('h2')`
   margin-top: 123px;
-  margin-bottom: 30px;
-`;
-
-export const Title = styled('h2')`
   font-size: 24px;
   line-height: 1.38;
   text-align: center;
   color: #111;
-  margin-bottom: 24px;
 
   @media screen and (min-width: 768px) {
     font-size: 48px;
     line-height: 1.38;
-    margin-bottom: 40px;
   }
 `;

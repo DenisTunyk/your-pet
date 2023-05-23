a0301577ca68cbf4ea5a67875a06060c2fbd689c
Dagaz <wlada801@gmail.com>
2023-05-23 12:23:16 +0300

    fix-HomePageStyles

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import { Backgraund, Page, Container, Title } from './Hero.styled';
+
+export const Hero = () => {
+  return (
+    <>
+      <Backgraund>
+        <Page>
+          <Container>
+            <Title>Take good care of your small pets</Title>
+          </Container>
+        </Page>
+      </Backgraund>
+    </>
+  );
+};
+
+export default Hero;

6d39a8de9726380400fbaeef56a160c576bb0c7f
Dagaz <wlada801@gmail.com>
2023-05-25 08:58:04 +0300

    fix

--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -147,7 +147,6 @@ export const Gif = styled.iframe`
   background-size: 200px auto;
   width: 250px;
   height: 150px;
-  /* background-position: top 500px; */
   @media screen and (min-width: 768px) {
     background-size: 350px auto;
     width: 450px;

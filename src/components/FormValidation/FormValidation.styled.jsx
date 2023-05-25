c82069e17ae95ae70c47605d3746aff1c3f670a2
VadimKorobec <_s_e_t__@ukr.net>
2023-05-25 15:54:09 +0300

    Update FormValidation.styled.jsx

--- src/components/FormValidation/FormValidation.styled.jsx
+++ src/components/FormValidation/FormValidation.styled.jsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
 export const ErrorText = styled.p`
+  position: absolute;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
   color: red;
+  margin-top: 4px;
+  margin-left: 20px;
 `;
 
 export const CorrectText = styled.p`
+  position: absolute;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
   color: green;
+  margin-top: 4px;
+  margin-left: 20px;
 `;

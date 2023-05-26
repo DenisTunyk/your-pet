5f24fb2fa2213178514a75f208e6718a47037553
VadimKorobec <_s_e_t__@ukr.net>
2023-05-26 17:30:11 +0300

    Update Spinner.jsx

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,3 +1,19 @@
+import { Rings } from 'react-loader-spinner';
+import { SpinnerWrapper } from './Spinner.styled';
+
 export const Spinner = () => {
-  return <div></div>;
+  return (
+    <SpinnerWrapper>
+      <Rings
+        height="80"
+        width="80"
+        color="#54ADFF"
+        radius="6"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="rings-loading"
+      />
+    </SpinnerWrapper>
+  );
 };

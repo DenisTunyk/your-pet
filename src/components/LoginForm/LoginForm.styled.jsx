2ef27ab6ee7cae1f12b23f6757e5bf02f7fd8b56
VadimKorobec <_s_e_t__@ukr.net>
2023-05-26 15:46:53 +0300

    Update LoginForm.styled.jsx

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -24,12 +24,19 @@ export const Container = styled.div`
 `;
 
 export const Titel = styled.h1`
+  font-weight: 400;
   font-size: 24px;
   line-height: 33px;
   align-items: center;
   text-align: center;
   color: #111111;
   margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 49px;
+  }
 `;
 
 export const FormAuth = styled(Form)`
@@ -106,6 +113,7 @@ export const Button = styled.button`
   margin-bottom: 24px;
 
   &:disabled {
+    background: #83c2fd;
     border: none;
     cursor: default;
   }

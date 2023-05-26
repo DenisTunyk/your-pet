5b1780fc9291a875db6c8b7377c493d35f74261b
Dagaz <wlada801@gmail.com>
2023-05-26 18:53:48 +0300

    fix

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,19 +1,13 @@
 import { NFText, NFImg, NFDivWrap, Button, PawIcon } from './NotFound.styled';
 import { Link } from 'react-router-dom';
 
-export const NotFound = ({ handleLinkClick }) => {
-  // const handleClick = () => {
-  //   if (handleLinkClick) {
-  //     handleLinkClick();
-  //   }
-  // };
+export const NotFound = () => {
   return (
     <>
       <NFDivWrap>
         <NFText>Oops! This page not found...</NFText>
         <NFImg />
-        {/* <Button type="submit"> */}
-        <Button>
+        <Button type="submit">
           <Link to="./">To main page</Link>
           <PawIcon />
         </Button>

a183eb1662dc3f81483666955cee5271eeb71856
Dagaz <wlada801@gmail.com>
2023-05-26 18:52:34 +0300

    add link to NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,14 +1,22 @@
-import { NFText, NFImg, NFDivWrap,Button, PawIcon} from './NotFound.styled';
+import { NFText, NFImg, NFDivWrap, Button, PawIcon } from './NotFound.styled';
+import { Link } from 'react-router-dom';
 
-
-export const NotFound = () => {
+export const NotFound = ({ handleLinkClick }) => {
+  // const handleClick = () => {
+  //   if (handleLinkClick) {
+  //     handleLinkClick();
+  //   }
+  // };
   return (
     <>
       <NFDivWrap>
-      <NFText>Oops! This page not found...</NFText>
+        <NFText>Oops! This page not found...</NFText>
         <NFImg />
-              <Button type="submit">To main page<PawIcon />
-              </Button>
+        {/* <Button type="submit"> */}
+        <Button>
+          <Link to="./">To main page</Link>
+          <PawIcon />
+        </Button>
       </NFDivWrap>
     </>
   );

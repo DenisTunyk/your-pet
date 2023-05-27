dedd251b24be8f829391c8ebef3f0dec3f68aa0c
VadimKorobec <_s_e_t__@ukr.net>
2023-05-27 22:29:16 +0300

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -3,4 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding-left: 20px;
   padding-right: 20px;
+  margin-top: 20px;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 32px;
+    padding-right: 32px;
+    margin-top: 24px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 `;

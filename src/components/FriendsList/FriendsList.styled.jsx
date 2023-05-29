9842ea851eb5e431c3301b50b8fc0fe452843097
VadimKorobec <_s_e_t__@ukr.net>
2023-05-29 16:26:36 +0300

    Update FriendsList.styled.jsx

--- src/components/FriendsList/FriendsList.styled.jsx
+++ src/components/FriendsList/FriendsList.styled.jsx
@@ -1,41 +1,43 @@
 import styled from 'styled-components';
 
-export const List = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  width: 100%;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 16px 12px;
+`;
 
-  @media screen and (min-width: 768px) {
-    display: flex;
-    flex-wrap: wrap;
-    column-gap: 32px;
-    row-gap: 32px;
+export const Title = styled.h2`
+  font-weight: ;
+  font-size: 24px;
+  line-height: 1.375;
+  color: ;
+  margin-bottom: 24px;
+
+  @media screen and () {
+    font-size: 48px;
+    margin-bottom: 40px;
+  }
+
+  @media screen and () {
+    margin-bottom: 60px;
   }
 `;
 
-export const Item = styled.li`
-  width: 336px;
-  height: 275px;
-  margin-bottom: 12px;
-  background-color: white;
-  border-radius: 20px;
-  padding: 12px 5px 12px;
+export const CardList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  row-gap: 20px;
 
-  box-shadow: 7px 4px 14px 0px #31150412;
-
-  &:last-child {
-    margin-bottom: 0;
+  @media screen and () {
+    row-gap: 24px;
+    column-gap: 32px;
   }
 
-  @media screen and (min-width: 768px) {
-    margin-bottom: 0;
-    padding: 16px 5px 16px;
-    border-radius: 40px;
-    flex-basis: calc((100% - 32px) / 2);
-  }
-
-  @media screen and (min-width: 1280px) {
-    flex-basis: calc((100% - 64px) / 3);
+  @media screen and () {
+    row-gap: 20px;
   }
 `;

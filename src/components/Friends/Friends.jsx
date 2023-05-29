9adc85674b9eed1f36453c824f8b2a694b18399d
VadimKorobec <_s_e_t__@ukr.net>
2023-05-29 15:46:59 +0300

    Update Friends.jsx

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,11 +1,13 @@
 import { FriendList } from 'components/FriendsList/FriendsList';
-import { OurFriendsWrapper, Titel } from './Friends.styled';
+import { Container, OurFriendsWrapper, Titel } from './Friends.styled';
 
 export const Friends = () => {
   return (
     <OurFriendsWrapper>
-      <Titel>Our friends</Titel>
-      <FriendList />
+      <Container>
+        <Titel>Our friends</Titel>
+        <FriendList />
+      </Container>
     </OurFriendsWrapper>
   );
 };

38765ba04f7634e71959c5fafa56b12f22a853d7
VadimKorobec <_s_e_t__@ukr.net>
2023-05-29 14:38:38 +0300

    Update friends.js

--- src/api/friends.js
+++ src/api/friends.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const getFriends = async () => {
-  const response = await axios.get();
+  const response = await axios.get('/api/sponsors');
+  return response;
 };

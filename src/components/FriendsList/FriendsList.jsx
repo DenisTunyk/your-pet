304a9882e961f7b334fd9b72352127d53e018df5
VadimKorobec <_s_e_t__@ukr.net>
2023-05-29 14:38:36 +0300

    Update FriendsList.jsx

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,13 +1,30 @@
 import { FriendListItem } from 'components/FriendsListItem/FriendsListItem';
 import { List, ListItem, Titel } from './FriendsList.styled';
+import { useState } from 'react';
+import { getFriends } from 'api/friends';
+import { useEffect } from 'react';
 
 export const FriendList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const result = await getFriends();
+        setData([...result.data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFriends();
+  }, []);
+
   return (
     <div>
       <Titel>Our friends</Titel>
       <List>
-        {friends.map(friend => (
-          <ListItem key={friend.id}>
+        {data.map(friend => (
+          <ListItem key={friend._id}>
             <FriendListItem friend={friend} />
           </ListItem>
         ))}

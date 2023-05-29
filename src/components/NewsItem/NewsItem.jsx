3ccc0a3a20aa758e6cf15833b04bfec6a4adaed9
Dagaz <wlada801@gmail.com>
2023-05-29 09:05:01 +0300

    addSearch

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,24 +1,58 @@
 import React, { useState } from 'react';
 import newsData from './news.json';
-import { Card, Wrapper, Title, Content, Link } from './NewsItem.styled';
+import {
+  Card,
+  Wrapper,
+  Title,
+  Content,
+  Link,
+  WrapImg,
+  Img,
+  Input,
+} from './NewsItem.styled';
 
 export const NewsItem = () => {
-  const [news] = useState(newsData.news);
+  // const [news] = useState(newsData.news);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredNews = newsData.news.filter(item =>
+    item.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearch = event => {
+    setSearchQuery(event.target.value);
+  };
 
   return (
-    <Wrapper>
-      {news.map((item, id) => (
-        <Card key={id}>
-          <Title>{item.title}</Title>
-          <Content>{item.text}</Content>
-          <p>{item.date}</p>
-          <Link href={item.url} target="blank">
-            Read more
-          </Link>
-          <hr />
-        </Card>
-      ))}
-    </Wrapper>
+    <>
+      <Input
+        type="text"
+        value={searchQuery}
+        onChange={handleSearch}
+        placeholder="Поиск новостей"
+      />
+      <Wrapper>
+        {filteredNews.map((item, id) => (
+          <Card key={id}>
+            <WrapImg>
+              <Img
+                src={item.imgUrl}
+                alt={item.title}
+                loading="lazy"
+                width="280"
+              />
+            </WrapImg>
+            <Title>{item.title}</Title>
+            <Content>{item.text}</Content>
+            <p>{item.date}</p>
+            <Link href={item.url} target="blank">
+              Read more
+            </Link>
+            <hr />
+          </Card>
+        ))}
+      </Wrapper>
+    </>
   );
 };
 
--- src/components/NewsItem/NewsItem.styled.jsx
+++ src/components/NewsItem/NewsItem.styled.jsx
@@ -11,6 +11,18 @@ export const Card = styled.div`
   height: 578px;
   margin-right: auto;
   margin-left: auto;
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -20px;
+    left: 0;
+    width: 100%;
+    height: 8px;
+    border-radius: 4px;
+    background-color: blue;
+    background: blue;
+  }
 `;
 
 export const Title = styled.p`
@@ -32,6 +44,29 @@ export const Link = styled.a`
   font-weight: 500;
   font-size: 16px;
   line-height: 22px;
+`;
+
+export const WrapImg = styled.div`
+  overflow: hidden;
+  border-radius: 20px;
+  object-fit: cover;
+  width: 100%;
+  height: 252px;
+`;
+
+export const Img = styled.img`
+  display: block;
+  height: 100%;
+  width: 100%;
+`;
+export const Input = styled.input`
+  height: 44px;
+  box-shadow: rgba(136, 198, 253, 0.19) 3px 8px 14px;
+  border-radius: 40px;
+  border: 1px solid transparent;
+  padding: 8px 20px;
+  box-sizing: border-box;
+  outline: none;
 `;
 
 export default Card;

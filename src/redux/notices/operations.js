42655cd9eb131cb3decfa923db2b4e148733eb8a
OleksiiChernii <oleksii.chernii@gmail.com>
2023-05-29 13:49:31 +0300

    removed console.log

--- src/redux/notices/operations.js
+++ src/redux/notices/operations.js
@@ -114,7 +114,6 @@ export const addFavoriteNotice = createAsyncThunk(
   async (pet, { rejectWithValue }) => {
     try {
       const response = await axios.patch(`/api/notices/favorite/${pet._id}`);
-      console.log(response.data)
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);

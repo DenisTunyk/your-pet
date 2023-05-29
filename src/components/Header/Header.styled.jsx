da21a49c5e3b394616c94609ffdfc8c664e9392b
OleksiiChernii <oleksii.chernii@gmail.com>
2023-05-29 20:28:19 +0300

    profile styles

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
-  display: flex;
+  width: 320px;
+  padding: 0 20px;
   margin: 0 auto;
-  top: 0;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #fdf7f2;
 
-  /* @media screen and (min-width: 768px) {
+  @media (min-width: 768px) {
     width: 768px;
-  } */
+    padding: 0 33px;
+  }
 
   @media screen and (min-width: 1280px) {
     width: 1280px;
+    padding: 0 16px;
   }
 `;
